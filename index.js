6b76e291fa215afebd62931bc9158d0b9bf398f0
lottie-em <charlottenash15@gmail.com>
2015-02-20 15:51:31 +0000

    twitter get request now pushes to database (first stage

--- index.js
+++ index.js
@@ -7,15 +7,23 @@ var http = require('http'),
 	Twitter = require('twitter'),
 	events = require('events'),
 	insta = require('instagram-node').instagram(),
-	tweets,
+	tweetObj = {},
 	// key = require('./key.js');
 	key = new Twitter ({
 	consumer_key: '0NE1Xoy4LOwiRuPBQYOZxAdsy',
 	consumer_secret: 'FwlWrejjFsJzh57JSd1OXlVFi2djcghERQNhHWsYZkgCLKUVxZ',
 	access_token_key: '281746499-SfqRkJFWWD447WgouP184FlS1EkJ6CTJajYT5G0A',
 	access_token_secret: 'uzLRrD1ZAi2UgNk12kK7F6HQq4AXZNW8rG8mbsR2z6WsI'
-}),
+	}),
 	db = mongojs('mongodb://lottie-em:arcjrules@ds039281.mongolab.com:39281/arcj', ['twitterUsers']);
+	type = 'search/tweets',
+	params = {
+		q: 'fruit',
+		filter: 'images',
+		include_entities: true,
+		result_type: 'images'
+	},
+	tweetArray = [];
 
 function requestHandler(request, response){
 	var reqName = path.basename(request.url) || 'index.html';
@@ -39,29 +47,37 @@ function requestHandler(request, response){
 http.createServer(requestHandler).listen(1337);
 console.log('Server up and running');
 
+//TWITTER
 
+// catchfish sends get req to twitter for the param
 function catchFish(){
-	var type = 'search/tweets';
-	var params = {q: 'fruit'};
-	key.get(type, params, function(error, data, response){
-		for(var i = 0; i < data.statuses.length; i += 1) {
-			console.log(data.statuses[i].text);
-			tweets = data.statuses[i].text;
-			pushToDb(data, "twitterUsers");
-
-		}
-	});
+	key.get(type, params, fishGutter);
 }
 
-catchFish();
+//fishgutter pulls the tweets from the json obj and adds to twitter object
+function fishGutter(error, data, response){
+		for(var i = 0; i < data.statuses.length; i += 1) {
+			tweetObj = {
+				tweetStatus: data.statuses[i].text,
+				tag: params.q,
+				mediaUrl: data.statuses[i].entities.media[0].media_url
+			}	
+			tweetArray.push(tweetObj);
+		}
+		pushToDb(tweetArray, "twitterUsers");
+	}
+
+//DB
+
+// push info to database
 
 function pushToDb(info, destination) {
 	db[destination].insert(info);
 }
+ 
+// start the function chain 
 
-// pushToDb({username: "mario", topInterest: "bunnies"}, "twitterUsers");
+catchFish();
 
 
-// db.twitterUsers.find(function(err, docs){
-// 	console.log(docs)
-// });
+
